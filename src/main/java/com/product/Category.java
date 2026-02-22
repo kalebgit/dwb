786a9dde3b13b0afb1d76eb07ebe56091bc00789
kal <emilianokaleb@gmail.com>
2026-02-22 07:58:16 -0600

    feat/fix(): nueva version en blanco del proyecto pues se cambio de gradlew a maven

--- src/main/java/com/product/Category.java
+++ src/main/java/com/product/Category.java
@@ -8,20 +8,41 @@ public class Category {
 
     public Category(Integer category_id, String category, String tag, Integer status) {
         this.category_id = category_id;
-        this.category   = category;
-        this.tag        = tag;
-        this.status     = status;
+        this.category = category;
+        this.tag = tag;
+        this.status = status;
     }
-    public Integer getCategory_id() { return category_id; }
-    public void setCategory_id(Integer category_id) { this.category_id = category_id; }
 
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
+    public Integer getCategory_id() {
+        return category_id;
+    }
 
-    public String getTag() { return tag; }
-    public void setTag(String tag) { this.tag = tag; }
+    public void setCategory_id(Integer category_id) {
+        this.category_id = category_id;
+    }
 
-    public Integer getStatus() { return status; }
-    public void setStatus(Integer status) { this.status = status; }
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
 }
