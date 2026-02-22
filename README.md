1bc988cd456df2e2ed00cad3f8932bdf0fd8a102
kal <emilianokaleb@gmail.com>
2026-02-22 07:58:16 -0600

    feat/fix(): nueva version en blanco del proyecto pues se cambio de gradlew a maven

--- README.md
+++
@@ -1,2 +0,0 @@
-# dwb
-repositorio donde se guardara todo lo relacionado a Seminario de Ciencias de la Computacion B [Desarrollo Web Backend]
--- src/main/java/kal/com/Category.java
+++
@@ -1,8 +0,0 @@
-package kal.com;
-
-public class Category {
-    Integer category_id;
-    String category;
-    String tag;
-    Integer status;
-}
