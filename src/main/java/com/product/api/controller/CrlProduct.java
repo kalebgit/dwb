4fb898d4298d2067145ca8fbdefa5d2900382a48
kal <emilianokaleb@gmail.com>
2026-03-06 21:21:20 -0600

    feat(): funcionalidad de jpa ya funciona y la inyeccino de depdencias tambien con repository y el nuevo service

--- src/main/java/com/product/api/controller/CrlProduct.java
+++ src/main/java/com/product/api/controller/CrlProduct.java
@@ -2,16 +2,19 @@ package com.product.api.controller;
 
 import com.product.CrudCategory;
 import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CrlProduct {
 
-    private CrudCategory crudCategory = new CrudCategory();
+    @Autowired
+    SvcCategory svcCategory;
 
     @GetMapping("/category")
     public Category[] getCategories(){
-        return crudCategory.getCategories();
+        return svcCategory.getCategories().toArray(new Category[0]);
     }
 }
---
+++ src/main/java/com/product/api/repository/RepoCategory.java
@@ -0,0 +1,14 @@
+package com.product.api.repository;
+
+import com.product.api.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RepoCategory extends JpaRepository<Category, Integer> {
+
+    @Query(value="SELECT * FROM category ORDER BY category_id", nativeQuery = true)
+    public List<Category> getCategories();
+    
+}
