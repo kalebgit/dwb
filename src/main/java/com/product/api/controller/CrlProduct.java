c4ba40264274020ac7bd241c59684b18f1491085
kal <emilianokaleb@gmail.com>
2026-03-13 21:01:22 -0600

    Revert "feat(): funcionalidad de jpa ya funciona y la inyeccino de depdencias tambien con repository y el nuevo service"
    This reverts commit 4fb898d4298d2067145ca8fbdefa5d2900382a48.

--- src/main/java/com/product/api/controller/CrlProduct.java
+++ src/main/java/com/product/api/controller/CrlProduct.java
@@ -2,19 +2,16 @@ package com.product.api.controller;
 
 import com.product.CrudCategory;
 import com.product.api.entity.Category;
-import com.product.api.service.SvcCategory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CrlProduct {
 
-    @Autowired
-    SvcCategory svcCategory;
+    private CrudCategory crudCategory = new CrudCategory();
 
     @GetMapping("/category")
     public Category[] getCategories(){
-        return svcCategory.getCategories().toArray(new Category[0]);
+        return crudCategory.getCategories();
     }
 }
